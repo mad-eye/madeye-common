241a4cec0497abd9150eac1413ca77bc80f4d6e0
Mike Risse <rissem@gmail.com>
2012-11-22 23:32:54 -0800

    export madeye settings to apogee server

--- package.js
+++ package.js
@@ -3,7 +3,14 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+
+//is this necessary, found in meteor packages
+//where = where || ["client", "server"];
+//but absent in atmosphere packages 
+
   api.use("underscore", "client");
-  api.add_files(["preMeteor.js", "dist/fileTree.js", "postMeteor.js"], "client");
-  api.add_files(["preMeteor.js", "postMeteor.js"], "server");
+  api.add_files(["preMeteor.js"], ["client", "server"]);
+  api.add_files(["dist/fileTree.js"], "client");
+  api.add_files(["dist/Settings.js"], "server");
+  api.add_files(["postMeteor.js"], ["client", "server"]);
 });
