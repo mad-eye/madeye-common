79f984626742a7319b6452ce2ebaaa8042e1de70
Arya <arya@madeye.io>
2013-11-08 01:56:07 +0000

    remove semver dependency

--- package.js
+++ package.js
@@ -2,10 +2,6 @@ Package.describe({
   summary: "common files shared by madeye projects"
 });
 
-Npm.depends({
-  semver: '2.1.0' //Used by apogee server
-});
-
 Package.on_use(function (api, where) {
 
   api.use(["coffeescript", 'underscore'], ["client", "server"]);
